ca993bcf57bc03f82b661fa2f60a3046c287b51a
Arsenii Tsubera <111arsen111@gmail.com>
2023-08-07 14:33:50 +0300

    feat: setup art gallery

--- src/ui/sections/art-section.jsx
+++ src/ui/sections/art-section.jsx
@@ -1,3 +1,41 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { EffectFade, Navigation, Pagination } from "swiper/modules";
+
 export function ArtSection() {
-  return <div className="w-full max-w-[1000px]">Mysh</div>;
+  return (
+    <div className="w-full max-w-[1000px] h-[2200px] flex flex-col gap-5 jusify-center items-center">
+      <div className="text-5xl">Some of my artworks</div>
+      <>
+        <Swiper
+          spaceBetween={30}
+          effect={"fade"}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[EffectFade, Navigation, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="d" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="d" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="d" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+          </SwiperSlide>
+        </Swiper>
+      </>
+    </div>
+  );
 }
